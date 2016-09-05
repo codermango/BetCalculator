14df8efec1598df37a7d6971cee3a4d176f48ea5
codermango <codermango@gmail.com>
2016-09-05 23:00:32 +0200

    Validate if input is number

--- app/components/CellTextField/index.js
+++ app/components/CellTextField/index.js
@@ -18,22 +18,51 @@ class CellTextField extends React.Component { // eslint-disable-line react/prefe
 
   static propTypes = {
     label: React.PropTypes.string,
+    width: React.PropTypes.string,
   };
 
+  checkInput = (e) => {
+    const textValue = Number(e.target.value);
+    console.log(textValue);
+    if (Number.isNaN(textValue)) {
+      this.setState({
+        label: 'Please input number!',
+        floatingLabelFocusColor: 'red',
+      });
+    } else {
+      this.setState({
+        label: this.props.label,
+        floatingLabelFocusColor: 'rgb(0, 188, 212)',
+      });
+    }
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      label: this.props.label,
+      floatingLabelFocusColor: 'rgb(0, 188, 212)',
+    };
+  }
+
   render() {
-    const { width, label } = this.props;
+    const { width } = this.props;
     return (
       <div className={styles.cellTextField} style={{ width: width }}>
         <TextField
-          floatingLabelText={label}
+          floatingLabelText={this.state.label}
           style={{
             fontSize: '0.8vw',
             width: '80%',
+          }}
+          floatingLabelFocusStyle={{
+            color: this.state.floatingLabelFocusColor
           }}
           inputStyle={{
             margin: '0',
             padding: '20px 0 0 0',
           }}
+          onChange={(e) => this.checkInput(e)}
         />
       </div>
     );
